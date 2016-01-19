f94ef15591f49a56425bf01cd34a8536a208cbfc
Kyler Markland <16.kyler.markland@ksd.org>
2016-01-19 08:18:09 -0800

    Removed old method comments

--- src/main/filter/Grayscale.java
+++ src/main/filter/Grayscale.java
@@ -12,19 +12,6 @@ public class Grayscale extends Filter
 	@Override
 	public void modifyImage(DrawEvent e)
 	{
-		// for (int i = 0; i < img.getWidth(); i++)
-		// {
-			// for (int j = 0; j < img.getHeight(); j++)
-			// {
-				// int rgb   = img.getRGB(i, j);
-				// int red   = (rgb >> 16) & 0xff;
-				// int green = (rgb >> 8) & 0xff;
-				// int blue  = rgb & 0xff;
-				// int ave = (red + green + blue) / 3;
-				// img.setRGB(i, j, (ave << 16) + (ave << 8) + ave);
-			// }
-		// }
-		
 		BufferedImage img = e.getManager().getCurrentLayer().getImage();
 		Area area = e.getArea();
 		Rectangle bounds = area.getBounds();
